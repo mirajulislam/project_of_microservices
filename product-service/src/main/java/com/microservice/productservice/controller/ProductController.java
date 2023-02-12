5f61a11a9678bfb3577035d65ec3ad07d28c27a5
miraj <mirajulislam5746@gmail.com>
2023-02-12 11:35:11 +0600

    formate

--- product-service/src/main/java/com/microservice/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/microservice/productservice/controller/ProductController.java
@@ -20,19 +20,19 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("api/product")
 @RequiredArgsConstructor
 public class ProductController {
-	
-	 private final ProductService productService;
-	
+
+	private final ProductService productService;
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public void createProduct(@RequestBody ProductRequest productRequest) {
 		productService.createProduct(productRequest);
 	}
-	
-	 @GetMapping
-	 @ResponseStatus(HttpStatus.OK)
-	 public List<ProductResponse> getAllProducts() {
-	    return productService.getAllProducts();
-	 }
+
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public List<ProductResponse> getAllProducts() {
+		return productService.getAllProducts();
+	}
 
 }
