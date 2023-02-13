9b521a56a01538ea04ad1acceec89ac17ca101de
miraj <mirajulislam5746@gmail.com>
2023-02-13 11:18:19 +0600

    load balancing and api call discovery service wise

--- order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class OrderService {
 	private final OrderRepository orderRepository;
-	private final WebClient webClient;
+	private final WebClient.Builder webClientBuilder;
 	
 	 public void placeOrder(OrderRequest orderRequest) {
 	        Order order = new Order();
@@ -38,8 +38,8 @@ public class OrderService {
 	                .toList();
 	        // Call Inventory Service, and place order if product is in
 	        // stock
-	        InventoryResponse[] inventoryResponsArray = webClient.get()
-	        		.uri("http://localhost:8082/api/inventory",
+	        InventoryResponse[] inventoryResponsArray = webClientBuilder.build().get()
+	        		.uri("http://inventory-service/api/inventory",
 	                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
 	                .retrieve()
 	                .bodyToMono(InventoryResponse[].class)
